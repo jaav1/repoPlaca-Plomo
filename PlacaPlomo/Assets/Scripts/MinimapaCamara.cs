1e034e105a75acd05e96b407c94c7a8fb4347570
Eduardo6814 <leduardo.argueta@gmail.com>
2025-08-26 20:08:10 -0600

    Mini mapa
    Hice el mini mapa

---
+++ PlacaPlomo/Assets/Scripts/MinimapaCamara.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MinimapaCamara : MonoBehaviour
+{
+    public Transform player;
+
+    private void LateUpdate()
+    {
+        
+        Vector3 newPosition = player.position; 
+        
+        newPosition.y = transform.position.y;
+
+        transform.position = newPosition;
+
+    }
+}
